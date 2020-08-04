1a96e1d716c44edc453d1a10d31325aad19ae2e3
AJ Gambino <aj.gambino7@gmail.com>
2020-08-04 16:09:24 -0400

    6/10 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+const nav = document.querySelector(".main-navigation");
+const intro = document.querySelector(".intro");
+const copyright = document.querySelector(".footer");
+const buttons = document.querySelector(".btn");
+const content = document.querySelector(".text-content");
+const destination = document.querySelector(".content-destination");
+
+nav.addEventListener("mouseover", () => {
+  nav.style.backgroundColor = "red";
+});
+
+intro.addEventListener("click", () => {
+  intro.style.color = "red";
+});
+
+document.addEventListener("keydown", () => {
+  if (event.keyCode === 66) {
+    copyright.style.fontWeight = "bold";
+  }
+});
+
+document.addEventListener("scroll", () => {
+  nav.style.backgroundColor = "lightblue";
+});
+
+window.addEventListener("load", (event) => {
+  content.style.color = "green";
+});
+
+window.addEventListener("resize", () => {
+  destination.style.backgroundColor = "yellow";
+});
